d8f5c646890effff616eaf7f60273bea17a5ee90
George Camboim <george.camboim@embedded.ufcg.edu.br>
2019-02-27 15:20:03 -0300

    Desenvolvimento de unidade de controle

--- blocks/ri5cy/frontend/rtl/src/control_unit.sv
+++ blocks/ri5cy/frontend/rtl/src/control_unit.sv
@@ -6,22 +6,45 @@
 module control_unit
 	(
 		input  logic [WORD_WIDTH-1:0]		instr_i,
-
-		// Sinais ULA
-		output logic										neg_mux_ctrl_o,
+		// Saídas de controle
 		output logic [ALU_OP_WIDTH-1:0] alu_op_ctrl_o,
-
-		output logic										regwrite_en_o
+		output logic										write_en_ctrl_o,
+		output logic										imm_ctrl_o,
+		output logic										stype_ctrl_o,
+		output logic										upper_ctrl_o,
+		output logic										lui_shift_ctrl_o,
+		output logic										pc_ula_ctrl_o,
+		output logic										load_ctrl_o,
+		output logic										store_ctrl_o,
+		output logic										branch_ctrl_o,
+		output logic										brn_inv_ctrl_o,
+		output logic										jal_ctrl_o,
+		output logic										jalr_ctrl_o
 	);
+
+	logic [DCODE_WIDTH-1:0] decoded_op;
 
 	decoder decoder
 		(
-
+			.instr_i(instr_i),
+			.decoded_op_o(decoded_op)
 		);
 
 	controller ctrlr
 		(
-
+			.decoded_op_i(decoded_op),
+			.write_en_ctrl_o,
+			.imm_ctrl_o,
+			.stype_ctrl_o,
+			.upper_ctrl_o,
+			.lui_shift_ctrl_o,
+			.pc_ula_ctrl_o,
+			.load_ctrl_o,
+			.store_ctrl_o,
+			.branch_ctrl_o,
+			.brn_inv_ctrl_o,
+			.jal_ctrl_o,
+			.jalr_ctrl_o
 		);
 
 endmodule
