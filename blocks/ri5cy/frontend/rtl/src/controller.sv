4ee1f581d97b43822295ff4b356258acea68ac23
George Camboim <george.camboim@embedded.ufcg.edu.br>
2019-02-27 15:19:32 -0300

    Desenvolvimento de controlador

--- blocks/ri5cy/frontend/rtl/src/controller.sv
+++ blocks/ri5cy/frontend/rtl/src/controller.sv
@@ -113,17 +113,176 @@ module controller
 
 		always_comb begin
 			case(operation)
-				ADD:
-				SUB:
-				SLL:
-				SRL:
-				SRA:
-				AND:
-				OR:
-				XOR:
-				SLT:
-				SLTU:
+				ADD, ADDI, AUIPC, LB, LBU, LH, LHU, LW, SB, SH, SW, JAL, JALR:
+					alu_op_ctrl_o = ALU_ADD;
+				SUB, BEQ, BNE:
+					alu_op_ctrl_o = ALU_SUB;
+				SLT, SLTI, BLT, BGE:
+					alu_op_ctrl_o = ALU_SLT;
+				SLTU, SLTIU, BLTU, BGEU:
+					alu_op_ctrl_o = ALU_SLTU;
+				OR, ORI:
+					alu_op_ctrl_o = ALU_OR;
+				AND, ANDI:
+					alu_op_ctrl_o = ALU_AND;
+				XOR, XORI:
+					alu_op_ctrl_o = ALU_XOR;
+				SLL, SLLI, LUI:
+					alu_op_ctrl_o = ALU_SLL;
+				SRL, SRLI:
+					alu_op_ctrl_o = ALU_SRL;
+				SRA, SRAI:
+					alu_op_ctrl_o = ALU_SRA;
+				default:
+					alu_op_ctrl_o = ALU_ADD;
+			endcase
+		end
 
+		always_comb begin
+			case(operation)
+				ADD, SUB, SLL, SRL, SRA, AND, OR, XOR, SLT, SLTU:
+					write_en_ctrl_o		= 1'b1;
+					imm_ctrl_o				= 1'b0;
+					stype_ctrl_o			= 1'b0;
+					upper_ctrl_o			= 1'b0;
+					lui_shift_ctrl_o	= 1'b0;
+					pc_ula_ctrl_o			= 1'b0;
+					load_ctrl_o				= 1'b0;
+					store_ctrl_o			= 1'b0;
+					branch_ctrl_o			= 1'b0;
+					brn_inv_ctrl_o		= 1'b0;
+					jal_ctrl_o 				= 1'b0;
+					jalr_ctrl_o 			= 1'b0;
+				ADDI, SLLI, SRLI, SRAI, ANDI, ORI, XORI, SLTI, SLTIU:
+					write_en_ctrl_o		= 1'b1;
+					imm_ctrl_o				= 1'b1;
+					stype_ctrl_o			= 1'b0;
+					upper_ctrl_o			= 1'b0;
+					lui_shift_ctrl_o	= 1'b0;
+					pc_ula_ctrl_o			= 1'b0;
+					load_ctrl_o				= 1'b0;
+					store_ctrl_o			= 1'b0;
+					branch_ctrl_o			= 1'b0;
+					brn_inv_ctrl_o		= 1'b0;
+					jal_ctrl_o 				= 1'b0;
+					jalr_ctrl_o 			= 1'b0;
+				LUI: // Tem que mudar essa porra
+					write_en_ctrl_o		= 1'b1;
+					imm_ctrl_o				= 1'b0;
+					stype_ctrl_o			= 1'b0;
+					upper_ctrl_o			= 1'b1;
+					lui_shift_ctrl_o	= 1'b1;
+					pc_ula_ctrl_o			= 1'b0;
+					load_ctrl_o				= 1'b0;
+					store_ctrl_o			= 1'b0;
+					branch_ctrl_o			= 1'b0;
+					brn_inv_ctrl_o		= 1'b0;
+					jal_ctrl_o 				= 1'b0;
+					jalr_ctrl_o 			= 1'b0;
+				AUIPC:
+					write_en_ctrl_o		= 1'b1;
+					imm_ctrl_o				= 1'b1;
+					stype_ctrl_o			= 1'b0;
+					upper_ctrl_o			= 1'b1;
+					lui_shift_ctrl_o	= 1'b0;
+					pc_ula_ctrl_o			= 1'b1;
+					load_ctrl_o				= 1'b0;
+					store_ctrl_o			= 1'b0;
+					branch_ctrl_o			= 1'b0;
+					brn_inv_ctrl_o		= 1'b0;
+					jal_ctrl_o 				= 1'b0;
+					jalr_ctrl_o 			= 1'b0;
+				LB, LBU, LH, LHU, LW:
+					write_en_ctrl_o		= 1'b1;
+					imm_ctrl_o				= 1'b1;
+					stype_ctrl_o			= 1'b0;
+					upper_ctrl_o			= 1'b0;
+					lui_shift_ctrl_o	= 1'b0;
+					pc_ula_ctrl_o			= 1'b0;
+					load_ctrl_o				= 1'b1;
+					store_ctrl_o			= 1'b0;
+					branch_ctrl_o			= 1'b0;
+					brn_inv_ctrl_o		= 1'b0;
+					jal_ctrl_o 				= 1'b0;
+					jalr_ctrl_o 			= 1'b0;
+				SB, SH, SW:
+					write_en_ctrl_o		= 1'b0;
+					imm_ctrl_o				= 1'b1;
+					stype_ctrl_o			= 1'b1;
+					upper_ctrl_o			= 1'b0;
+					lui_shift_ctrl_o	= 1'b0;
+					pc_ula_ctrl_o			= 1'b0;
+					load_ctrl_o				= 1'b0;
+					store_ctrl_o			= 1'b1;
+					branch_ctrl_o			= 1'b0;
+					brn_inv_ctrl_o		= 1'b0;
+					jal_ctrl_o 				= 1'b0;
+					jalr_ctrl_o 			= 1'b0;
+				BEQ, BLT, BLTU:
+					write_en_ctrl_o		= 1'b0;
+					imm_ctrl_o				= 1'b0;
+					stype_ctrl_o			= 1'b0;
+					upper_ctrl_o			= 1'b0;
+					lui_shift_ctrl_o	= 1'b0;
+					pc_ula_ctrl_o			= 1'b0;
+					load_ctrl_o				= 1'b0;
+					store_ctrl_o			= 1'b0;
+					branch_ctrl_o			= 1'b1;
+					brn_inv_ctrl_o		= 1'b0;
+					jal_ctrl_o 				= 1'b0;
+					jalr_ctrl_o 			= 1'b0;
+				BNE, BGE, BGEU:
+					write_en_ctrl_o		= 1'b0;
+					imm_ctrl_o				= 1'b0;
+					stype_ctrl_o			= 1'b0;
+					upper_ctrl_o			= 1'b0;
+					lui_shift_ctrl_o	= 1'b0;
+					pc_ula_ctrl_o			= 1'b0;
+					load_ctrl_o				= 1'b0;
+					store_ctrl_o			= 1'b0;
+					branch_ctrl_o			= 1'b1;
+					brn_inv_ctrl_o		= 1'b1;
+					jal_ctrl_o 				= 1'b0;
+					jalr_ctrl_o 			= 1'b0;
+				JAL:
+					write_en_ctrl_o		= 1'b1;
+					imm_ctrl_o				= 1'b0;
+					stype_ctrl_o			= 1'b0;
+					upper_ctrl_o			= 1'b0;
+					lui_shift_ctrl_o	= 1'b0;
+					pc_ula_ctrl_o			= 1'b1;
+					load_ctrl_o				= 1'b0;
+					store_ctrl_o			= 1'b0;
+					branch_ctrl_o			= 1'b1;
+					brn_inv_ctrl_o		= 1'b0;
+					jal_ctrl_o 				= 1'b1;
+					jalr_ctrl_o 			= 1'b0;
+				JALR: // Tem que mudar essa porra
+					write_en_ctrl_o		= 1'b1;
+					imm_ctrl_o				= 1'b1;
+					stype_ctrl_o			= 1'b0;
+					upper_ctrl_o			= 1'b0;
+					lui_shift_ctrl_o	= 1'b0;
+					pc_ula_ctrl_o			= 1'b0;
+					load_ctrl_o				= 1'b0;
+					store_ctrl_o			= 1'b0;
+					branch_ctrl_o			= 1'b0;
+					brn_inv_ctrl_o		= 1'b0;
+					jal_ctrl_o 				= 1'b0;
+					jalr_ctrl_o 			= 1'b0;
+				default:
+					write_en_ctrl_o		= 1'b1;
+					imm_ctrl_o				= 1'b1;
+					stype_ctrl_o			= 1'b0;
+					upper_ctrl_o			= 1'b0;
+					lui_shift_ctrl_o	= 1'b0;
+					pc_ula_ctrl_o			= 1'b0;
+					load_ctrl_o				= 1'b0;
+					store_ctrl_o			= 1'b0;
+					branch_ctrl_o			= 1'b0;
+					brn_inv_ctrl_o		= 1'b0;
+					jal_ctrl_o 				= 1'b0;
+					jalr_ctrl_o 			= 1'b0;
 			endcase
 		end
 
