dd33dda722f4666ce48a5c4cf7c1031c2af1383e
George Camboim <george.camboim@embedded.ufcg.edu.br>
2019-03-12 13:13:53 -0300

    Desenvolvendo registrador IF_to_ID

--- blocks/ri5cy/frontend/rtl/src/if_to_id.sv
+++ blocks/ri5cy/frontend/rtl/src/if_to_id.sv
@@ -1,9 +1,24 @@
 module IF_to_ID
   (
+    input  logic clk,
+    input  logic en,
+    input  logic rst_n,
 
+    input  logic [WORD_WIDTH-1:0] pc_i,
+    input  logic [WORD_WIDTH-1:0] pc_plus4_i,
+    input  logic [WORD_WIDTH-1:0] instruction_i,
 
+    output logic [WORD_WIDTH-1:0] pc_o,
+    output logic [WORD_WIDTH-1:0] pc_plus4_o,
+    output logic [WORD_WIDTH-1:0] instruction_o
   );
 
-
+  always_ff @(posedge clk) begin
+    if(en) begin
+      pc_o <= pc_i;
+      pc_plus4_o <= pc_plus4_i;
+      instruction_o <= instruction_i;
+    end
+  end
 
 endmodule
