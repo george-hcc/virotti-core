72c3622f8d05decb04beb381bdbb9a83767a67b0
George Camboim <george.camboim@embedded.ufcg.edu.br>
2019-03-15 11:09:55 -0300

    Instaciação de modulos do Core

--- blocks/ri5cy/frontend/rtl/src/id_stage.sv
+++ blocks/ri5cy/frontend/rtl/src/id_stage.sv
@@ -9,7 +9,7 @@ module id_stage
 		input  logic										rst_n,
 
 		// Vindo do estágio IF
-		input  logic [WORD_WIDTH-1:0]		instr_if_i,
+		input  logic [WORD_WIDTH-1:0]		instr_i,
 		input  logic [WORD_WIDTH-1:0]		pc_plus4_i,
 
 		// Saindo para estágio EX
@@ -21,7 +21,7 @@ module id_stage
 		input  logic [WORD_WIDTH-1:0]		wdata_wb_i,
 
 		// Sinais de controle
-		input  logic 										jal_mux_i,
+		input  logic 										jal_mux_i, // Checar Necessidade
 		input  logic										write_en_i,
 		output logic [ALU_OP_WIDTH-1:0]	alu_ctrl_o,
 		output logic										write_en_ctrl_o,
@@ -35,7 +35,8 @@ module id_stage
 		output logic										branch_ctrl_o,
 		output logic										brn_inv_ctrl_o,
 		output logic										jal_ctrl_o,
-		output logic										jalr_ctrl_o
+		output logic										jalr_ctrl_o,
+		output logic										md_op_ctrl_o
 	);
 
 	logic [ADDR_WIDTH-1:0]	addr_rs1;
@@ -43,8 +44,8 @@ module id_stage
 	logic [ADDR_WIDTH-1:0]  addr_rd;
 	logic [WORD_WIDTH-1:0]	wdata;
 
-	assign addr_rs1 = instr_if_i[19:15];
-	assign addr_rs2 = instr_if_i[24:20];
+	assign addr_rs1 = instr_i[19:15];
+	assign addr_rs2 = instr_i[24:20];
 	assign addr_rd  = waddr_wb_i;
 	assign wdata 		= (jal_mux_i) ? (pc_plus4_i) : (wdata_wb_i);
 
@@ -63,7 +64,7 @@ module id_stage
 
 	control_unit ctrl_unit 
 		(
-			.instr_i(instr_if_i),
+			.instr_i(instr_i),
 			.alu_op_ctrl_o(alu_ctrl_o),
 			.write_en_ctrl_o,
 			.imm_ctrl_o,
@@ -76,7 +77,8 @@ module id_stage
 			.branch_ctrl_o,
 			.brn_inv_ctrl_o,
 			.jal_ctrl_o,
-			.jalr_ctrl_o
+			.jalr_ctrl_o,
+			.md_op_ctrl_o
 		);
 
 endmodule
